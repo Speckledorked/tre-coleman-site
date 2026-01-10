52d4f05cda4edf43daeb0105d8ebc8d6335f20d9
Speckledorked <realclouderg@gmail.com>
2026-01-10 14:47:39 -0500

    Add Google Analytics tracking script

---
+++ analytics.js
@@ -0,0 +1,12 @@
+// Google Analytics
+(function() {
+    var script = document.createElement('script');
+    script.async = true;
+    script.src = 'https://www.googletagmanager.com/gtag/js?id=G-778929FT8G';
+    document.head.appendChild(script);
+
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', 'G-778929FT8G');
+})();
